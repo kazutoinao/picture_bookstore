b5e83facdd668697c1d01ebca6bb4916fa7913be
kazutoinao <kazutoinao@gmail.com>
2022-01-24 13:31:38 +0000

    add

---
+++ app/controllers/public/addresses_controller.rb
@@ -0,0 +1,46 @@
+class Public::AddressesController < ApplicationController
+    def  index
+         @address=Address.new
+         @addresses=current_customer.addresses
+    end
+
+    def  create
+      @address=Address.new(address_params)
+      @address.customer_id=current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+     @addresses=Address.all
+      render :index
+    end
+    end
+
+    def  edit
+      @address=Address.find(params[:id])
+    end
+
+    def  update
+      @address=Address.find(params[:id])
+    if @address.update(address_params)
+       redirect_to addresses_path(@address)
+    else
+       @address=Address.find(params[:id])
+       render :edit
+    end
+    end
+
+
+
+    def  destroy
+    address=Address.find(params[:id])
+    if address.customer_id == current_customer.id
+    address.destroy
+    redirect_to addresses_path
+    end
+    end
+
+    private
+    def address_params
+        params.require(:address).permit(:postal_code, :address, :name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,62 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'public/homes#top'
+ devise_for :admins, path: 'admin',controllers: {
+  sessions:      'admin/sessions',
+  passwords:     'admin/passwords',
+  registrations: 'admin/registrations'
+ }
+ scope module: :public do
+ resource :customers, only: [:show, :edit, :update] do
+      collection do
+        get 'unsubscribe'
+        patch 'withdraw'
+      end
+    end
+  end
+devise_for :customers, controllers: {
+  sessions:      'public/sessions',
+  passwords:     'public/passwords',
+  registrations: 'public/registrations'
+}
+
+# devise_for(
+#   :customers,
+#   path: 'customers',
+#   module: 'public',
+#   skip: [:registration]
+#   )
+
+#   devise_scope :customers do
+#     get 'customers/sign_up' => 'public/registrations#new'
+#     post 'customers' => 'public/registrations#create'
+#   end
+
+
+ namespace :admin do
+    get 'homes/top' => 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+ end
+
+    scope module: :public do
+    get 'homes/top' => 'homes#top'
+    get 'homes/about' => 'homes#about'
+    resources :items, only: [:index, :show]
+
+    resources :cart_items, only: [:index, :update, :create, :destroy] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post 'confirm'
+        get 'complete'
+      end
+    end
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
 end
